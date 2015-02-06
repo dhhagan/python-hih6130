1e2a21fd951bd191e95291cdc8b359aba72f309b
David Hagan <dhagan@mit.edu>
2015-02-06 13:30:01 -0500

    first attempt at library

---
+++ HIH6130/io.py
@@ -0,0 +1,45 @@
+import smbus
+import datetime
+import sys
+import warnings
+
+__all__ = ['HIH6130']
+
+
+class HIH6130:
+	def __init__(self, address = 0x27):
+		self.address = address
+
+		self.status = None
+		self.relativehumidity = None
+		self.temperature = None
+		self.buffer = None
+		self.timestamp = None
+
+		try:
+			self.i2c = smbus.SMBus(1)
+		except:
+			sys.exit("Could not start SMBus.")
+
+	def read(self):
+		'''
+			read from the I2C bus at address defined above.
+		'''
+		try:
+			self.buffer = self.i2c.read__i2c_block_data(self.address, 0, 4)
+		except:
+			warnings.warn("Could not read from i2c bus.")
+
+		# Set the timestamp for the measurement
+		self.timestamp = datetime.utcnow()
+
+		#Set the status of the sensor
+		self.status = self.buffer[0] >> 6 & 0x03
+
+		# Set the RH reading
+		self.relativehumidity = round(((self.buffer[0] & 0x3f) << 8 | self.buffer[1]) * 100.0 / (2**14 - 1), 2)
+
+		# Set the T reading
+		self.temperature = round((float((self.buffer[2] << 6) + (self.buffer[3] >> 2)) / (2**14 - 1)) * 165.0 - 40, 2)
+
+		return
---
+++ setup.py
@@ -0,0 +1,29 @@
+'''
+	Python library for the HIH6130 RHT sensor from Honeywell
+
+	Written by David H Hagan
+	February 2015
+	Contact: david@davidhhagan.com
+'''
+
+# /HIH6130/setup.py
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(name='HIH6130',
+      version='0.0.3',
+      description='Python library for interacting with the Honeywell HIH6130 RHT sensor.',
+      url='http://github.com/dhhagan/python-hih6130',
+      author='David H Hagan',
+      author_email='david@davidhhagan.com',
+      license='MIT',
+	  keywords=['HIH6130', 'relative humidity', 'temperature'],
+      packages=['HIH6130',
+			],
+	  install_requires=[
+		'smbus-cffi',
+	  ],
+      zip_safe=False)

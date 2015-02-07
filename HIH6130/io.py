3a27fc62a85accb7193479af5e0139f0081781ff
David Hagan <dhagan@mit.edu>
2015-02-07 10:42:26 -0500

    added error raising

--- HIH6130/io.py
+++ HIH6130/io.py
@@ -1,12 +1,13 @@
 import smbus
 from datetime import datetime
-import sys
-import warnings
 
 __all__ = ['HIH6130']
 
 
 class HIH6130:
+	'''
+		HIH6130() defines a new RHT sensor with default address of 0x27.
+	'''
 	def __init__(self, address = 0x27):
 		self.address = address
 		self.status = None
@@ -18,32 +19,20 @@ class HIH6130:
 		try:
 			self.i2c = smbus.SMBus(1)
 		except:
-			sys.exit("Could not start SMBus.")
-		
-
-		return
+			raise IOError("Could not find i2c device.")
 
 	def read(self):
 		'''
-			read from the I2C bus at address defined above.
+			read from the i2c bus at address defined above.
 		'''
-		
 		try:
 			self.buffer = self.i2c.read_i2c_block_data(self.address, 0, 4)
 		except:
-			sys.exit("Could not read from i2c bus.")
+			raise IOError("Could not read from i2c device located at %s." % self.address )
 		
-
-		# Set the timestamp for the measurement
 		self.timestamp = datetime.utcnow()
-
-		#Set the status of the sensor
 		self.status = self.buffer[0] >> 6 & 0x03
-
-		# Set the RH reading
 		self.rh = round(((self.buffer[0] & 0x3f) << 8 | self.buffer[1]) * 100.0 / (2**14 - 1), 2)
-
-		# Set the T reading
 		self.t = round((float((self.buffer[2] << 6) + (self.buffer[3] >> 2)) / (2**14 - 1)) * 165.0 - 40, 2)
 
 		return
